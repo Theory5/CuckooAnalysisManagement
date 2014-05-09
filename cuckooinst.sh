b0a37d64f8fc4bce05a63bf78b1e32696a9d93af
Theory5 <Theory5@users.noreply.github.com>
2014-05-09 12:40:18 -0400

    Create cuckooinst.sh

---
+++ cuckooinst.sh
@@ -0,0 +1,6 @@
+#This is a cuckoo installation bash script, designed to make my life easier by automating a large portion of the cuckoo install
+#This install script Is to be used on ubuntu server 12.04.4, has not been tested on a 14.04 install. 
+#more information can be found in teh cuckoo change log. 
+#built by Theory5 on 5/9/14
+#Use as you please, just give credit to the cuckoo guys and myself. (Cuckoo gets top billing, obviously).
+
