cb087d4811d8f860e0bb99ae3aea5b1e41a5f2da
Theory5 <Theory5@users.noreply.github.com>
2014-06-25 18:47:27 -0400

    Update vmpresto.sh

--- vmpresto.sh
+++ vmpresto.sh
@@ -9,10 +9,17 @@
 #ask for name
 read -p "Please specify name of system:" SYSN
 
-#check to ensure text is entered
-	if [ $SYSN == "" ]; then
-    echo "Please enter a name for your VM!"
-    else
-    
 
 sudo virt-install --connect qemu:///system -n vm10 -r 512 --vcpus=2 --disk path=/var/lib/libvirt/images/vm10.img,size=12 -c /dev/cdrom --vnc --noautoconsole --os-type linux --accelerate --network=bridge:br0 --hvm 
+
+#until loop to ensure all variables have values entered
+
+
+#ask for name
+read -p "Please specify name of VM being created:" VAR1
+until [ -n "${VAR1}" ]; do read -p "Please enter a name:" VAR1; done
+
+read -p "Please specify the amount of RAM you wish to allocate to your VM (In MB's):" VAR2
+until [ -n "${VAR2}" ]; do read -p "Please enter a number:" VAR2; done
+
+
