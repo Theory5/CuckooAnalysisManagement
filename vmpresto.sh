d48f53c870d16d834fa17e5c000162d6ebd8e308
Theory5 <Theory5@users.noreply.github.com>
2014-06-25 19:36:55 -0400

    Update vmpresto.sh

--- vmpresto.sh
+++ vmpresto.sh
@@ -33,11 +33,16 @@ until [ -n "${VAR4}" ]; do read -p "Please enter a location:" VAR4; done
 #read -p "This script is specifying the graphical option "--vnc" which means it opens a VNC server on the first free port 5900 or above" [ENTER] 
 
 read -p "Do you want to attempt to autoconnect to the vnc server on the guest? Y/n" $VAR5
-if [ VAR5 == "Y"];then
-
-else
+if [ VAR5 != "Y"];then
 VAR6=$(--noautoconsole)
 fi
+
+read -p "Please enter the OS type:"
+until [ -n "${VAR7}" ]; do read -p "Please enter a location:" VAR7; done
+
+read -p "Please enter the virtual network bridge:"
+until [ -n "${VAR8}" ]; do read -p "Please enter a location:" VAR8; done
+
 sudo virt-install --connect qemu:///system -n ${VAR1} -r ${VAR2} --vcpus=2 --disk path=/var/lib/libvirt/images/${VAR1}.img \
-size=${VAR3} -c ${VAR4} --vnc ${VAR6} --os-type linux --accelerate --network=bridge:br0 --hvm 
+size=${VAR3} -c ${VAR4} --vnc ${VAR6} --os-type ${VAR7} --accelerate --${VAR8} --hvm 
 
